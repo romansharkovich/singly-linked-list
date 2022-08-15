a47a05280ab72e08724aa699a6b2f40c3f741bce
Roman Sharkovich <roman.sharkovich@solbeg.com>
2022-08-15 20:04:49 +0200

    single linked list implementation

---
+++ SinglyLinkedList/LinkedItem.cs
@@ -0,0 +1,42 @@
+﻿namespace SinglyLinkedList
+{
+    /// <summary>
+    /// A class that describes an item in a linked list.
+    /// </summary>
+    /// <typeparam name="T"> type </typeparam>
+    public class LinkedItem<T>
+    {
+        /// <summary>
+        /// Saved list data
+        /// </summary>
+        public T ListData { get; set; }
+
+        /// <summary>
+        /// next item of linked list
+        /// </summary>
+        public LinkedItem<T> Next { get; set; }
+
+        /// <summary>
+        /// Creating new instance of linked list
+        /// </summary>
+        /// <param name="data"> saved data </param>
+        public LinkedItem(T data)
+        {
+            if (data == null)
+            {
+                throw new ArgumentNullException(nameof(data));
+            }
+
+            ListData = data;
+        }
+
+        /// <summary>
+        /// Convert object to string
+        /// </summary>
+        /// <returns> Saved data </returns>
+        public override string ToString()
+        {
+            return ListData.ToString();
+        }
+    }
+}
---
+++ SinglyLinkedList/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace SinglyLinkedList
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // creating single linked list obj
+            var list = new SinglyLinkedList<int>();
+
+            // Adding elements
+            list.Add(42);
+            list.Add(1);
+            list.Add(17);
+            list.Add(5);
+            list.Add(-69);
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+
+            list.Delete(17);
+
+            // after
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
